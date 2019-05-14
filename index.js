c39e89c8364106df4b66850de7aaaf17ef0246a5
Joe Ganobsik <jganobsik@gmail.com>
2019-05-14 14:12:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 function createPost() {
  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
+ let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
  
  let postTitle = document.getElementById("postTitle").value;
  let postAuthor = document.getElementById("postAuthor").value;
