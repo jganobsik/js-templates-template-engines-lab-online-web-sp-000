934369119947e24652ae369bbd8d3414affb072b
Joe Ganobsik <jganobsik@gmail.com>
2019-05-14 13:48:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+function createPost() {
+ let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+ let postTemplate = _.template(document.getElementById("post-template").innerHTML);
+ 
+ let postTitle = document.getElementById("postTitle").value;
+ let postAuthor = document.getElementById("postAuthor").value;
+ let post = document.getElementById("postBody").value;
+ let blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+ let commentsSection = commentsTemplate();
+ let postElement = document.getElementById("post");
+
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+}
+
+
+
+function postComment() {
+  let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  let commentText = document.getElementById("commentText").value;
+  let commenterName = document.getElementById("commenter").value;
+  let commentsSection = document.getElementById("comments");
+  
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
+}
