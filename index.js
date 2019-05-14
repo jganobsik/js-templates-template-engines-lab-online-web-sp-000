24ea35b30652e070fece9d563c68f7943dd0bb68
Joe Ganobsik <jganobsik@gmail.com>
2019-05-14 14:15:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ function createPost() {
 
 function postComment() {
   let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
-
   let commentText = document.getElementById("commentText").value;
   let commenterName = document.getElementById("commenter").value;
   let commentsSection = document.getElementById("comments");
-  
   commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
